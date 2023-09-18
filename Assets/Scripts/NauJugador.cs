ea3c66cbad4abd389dafd371aac299e9154bb949
Anas Assadek <anasassadek@ceroca.cat>
2023-09-18 18:45:40 +0200

    Moviment Horitzontal i Vertical nau

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -16,11 +16,28 @@ public class NauJugador : MonoBehaviour
     void Update()
     {
         float direccioHoritzontal = Input.GetAxisRaw("Horizontal");
+        float direccioVertical = Input.GetAxisRaw("Vertical");
         //Debug.Log("direcioHoritzontal=" + direcioHoritzontal);
 
-        Vector2 direccioIndicada = new Vector2(direccioHoritzontal, 0f);
+        Vector2 direccioIndicada = new Vector2(direccioHoritzontal, direccioVertical).normalized;
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        float anchura = spriteRenderer.bounds.size.x / 2;
+        float altura = spriteRenderer.bounds.size.y / 2;
+
+        float limitEsquerra = -Camera.main.orthographicSize * Camera.main.aspect + anchura;
+        float limitDreta = Camera.main.orthographicSize * Camera.main.aspect - anchura;
+        float limitInferior = -Camera.main.orthographicSize + altura;
+        float limitSuperior = Camera.main.orthographicSize - altura;
+
         Vector2 novaPos = transform.position; // Ens retorna la posisció actual de la nau.
         novaPos += direccioIndicada * _velNau * Time.deltaTime;
+
+
+        novaPos.x = Mathf.Clamp(novaPos.x, limitEsquerra, limitDreta);
+        novaPos.y = Mathf.Clamp(novaPos.y, limitInferior, limitSuperior);
+
         transform.position = novaPos;
+
     }
 }
